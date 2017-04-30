f17692d6afa7e8dc4a1d7a5261daf35ad0bd1f6e
K.P. Sullivan <MacabreSkull@gmail.com>
2017-04-30 10:15:32 -0600

    Added the files

---
+++ Calculator/Calculator/Assets/op+.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class op+ : MonoBehaviour {
+	// Use this for initialization
+
+	public void OnClick () {
+    eqOperator = "/";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
